900e6ba75a89dfb6e6bc98010505d0ac3f354eea
Chu A. <chu.amk22@gmail.com>
2026-01-03 20:20:23 +0100

    feat: update Header component with navigation links and styling adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,24 @@ import { Link } from "react-router";
 const Header = () => {
   const navStyle =
     "text-white font-medium hover:text-yellow-100 transition-colors duration-200";
+  const buttonStyle =
+    "px-4 py-2 bg-gradient-to-r from-orange-500 to-red-500 text-white font-semibold rounded-lg hover:from-orange-600 hover:to-red-600 transition-all duration-200";
 
   return (
     <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 py-6">
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <div className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
             <div className="text-3xl font-bold text-white">🍳</div>
             <h1 className="text-2xl font-bold text-white">Foodie</h1>
-          </div>
+          </Link>
 
           {/* Navigation */}
           <nav>
             <ul className="flex gap-8 list-none">
               <li>
-                <Link to="/" className={navStyle}>
+                <Link to="/home" className={navStyle}>
                   Home
                 </Link>
               </li>
@@ -33,8 +35,13 @@ const Header = () => {
                 </Link>
               </li>
               <li>
-                <Link to="/settings" className={navStyle}>
-                  Settings
+                <Link to="/community" className={navStyle}>
+                  Community
+                </Link>
+              </li>
+              <li>
+                <Link to={"/signin"} className={buttonStyle}>
+                  Sign In
                 </Link>
               </li>
             </ul>
