4b05b06d246115d781c244fc37d9ace604aa6464
Chu A. <chu.amk22@gmail.com>
2026-01-06 19:07:38 +0100

    feat: implement searchRecipes function to search recipes from API

--- src/services/recipes.service.js
+++ src/services/recipes.service.js
@@ -6,6 +6,20 @@ export const fetchRecipes = async () => {
     return res.recipes;
   } catch (error) {
     console.error("Error fetching recipes:", error);
-    throw error;
+  }
+};
+
+export const searchRecipes = async (query) => {
+  try {
+    const res = await api
+      .get("search", {
+        searchParams: {
+          q: query,
+        },
+      })
+      .json();
+    return res;
+  } catch (error) {
+    console.error("Error searching recipe", error);
   }
 };
