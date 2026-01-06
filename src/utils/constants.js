3bf2abe6ad6fef7daaa252257a7d4beb658b9c52
Chu A. <chu.amk22@gmail.com>
2026-01-06 17:10:57 +0100

    feat: add SOCIAL_LINKS constant with social media information

---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+import { Twitter, Github, Facebook, Linkedin } from "lucide-react";
+
+export const SOCIAL_LINKS = [
+  {
+    name: "Twitter",
+    url: "https://x.com/chu_codes",
+    icon: Twitter,
+  },
+  {
+    name: "Github",
+    url: "https://github.com/Chu29",
+    icon: Github,
+  },
+  {
+    name: "Facebook",
+    url: "https://web.facebook.com/abuemkezechu/",
+    icon: Facebook,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/chu-abuemkeze",
+    icon: Linkedin,
+  },
+];
