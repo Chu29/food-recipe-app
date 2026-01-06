e1449442d90657458f031e0c1d86622467f6d434
Chu A. <chu.amk22@gmail.com>
2026-01-06 17:07:59 +0100

    fix: refine Hero component styles and layout for improved UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const Hero = ({ email, setEmail, handleSubmit }) => {
   return (
     <div className="min-h-screen relative overflow-hidden">
-      <div className="relative max-w-7xl mx-auto px-4 my-24 sm:py-32">
+      <div className="relative max-w-7xl mx-auto px-4 sm:py-32">
         <div className="text-center">
           <div className="flex justify-center mb-6">
-            <div className="text-7xl"><img className="w-40 h-40" src="./hero.svg" alt="" /></div>
+            <div className="text-7xl">
+              <img className="w-40 h-40" src="./hero.svg" alt="" />
+            </div>
           </div>
-          <h1 className="text-5xl sm:text-6xl font-bold mb-6 bg-linear-to-r from-orange-400 to-red-400 bg-clip-text text-transparent">
+          <h1 className="text-5xl sm:text-6xl font-bold mb-6 bg-orange-500 bg-clip-text text-transparent">
             Discover Your Next Favorite Recipe
           </h1>
           <p className="text-xl text-gray-300 mb-8 max-w-2xl mx-auto">
@@ -27,14 +29,14 @@ const Hero = ({ email, setEmail, handleSubmit }) => {
             <button
               type="submit"
               onClick={handleSubmit}
-              className="px-10 py-3 bg-linear-to-r from-orange-500 to-red-500 text-white font-semibold rounded-lg hover:from-orange-600 hover:to-red-600 transition-all duration-200 shadow-lg hover:shadow-xl flex-1"
+              className="px-10 py-3 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition-all duration-200 shadow-lg hover:shadow-xl flex-1"
             >
               Get Started
             </button>
           </form>
-          <p className="text-sm text-gray-400">
+          {/* <p className="text-sm text-gray-400">
             No credit card required • Free forever
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
