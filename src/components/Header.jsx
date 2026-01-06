9783ffc07ae85a943aa7be67d05759040ffb6ee0
Chu A. <chu.amk22@gmail.com>
2026-01-06 18:47:31 +0100

    fix: update Header component styles and navigation links for consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router";
 
 const Header = () => {
   const navStyle =
-    "text-white font-medium hover:text-yellow-100 transition-colors duration-200";
+    "text-white font-semibold hover:text-yellow-100 transition-colors duration-200";
   const buttonStyle =
-    "px-4 py-2 bg-gradient-to-r from-orange-500 to-red-500 text-white font-semibold rounded-lg hover:from-orange-600 hover:to-red-600 transition-all duration-200";
+    "px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition-all duration-200";
 
   return (
-    <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg">
+    <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg sticky top-0 z-10 ">
       <div className="max-w-7xl mx-auto px-4 py-6">
         <div className="flex items-center justify-between">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2">
-            <div className="text-3xl font-bold text-white">🍳</div>
+            <div className="text-3xl font-bold text-white">
+              <img className="w-10 h-10" src="./hero.svg" alt="" />
+            </div>
             <h1 className="text-2xl font-bold text-white">Foodie</h1>
           </Link>
 
@@ -20,7 +22,7 @@ const Header = () => {
           <nav>
             <ul className="flex gap-8 list-none">
               <li>
-                <Link to="/home" className={navStyle}>
+                <Link to="/" className={navStyle}>
                   Home
                 </Link>
               </li>
@@ -40,7 +42,7 @@ const Header = () => {
                 </Link>
               </li>
               <li>
-                <Link to={"/signin"} className={buttonStyle}>
+                <Link to="/signin" className={buttonStyle}>
                   Sign In
                 </Link>
               </li>
