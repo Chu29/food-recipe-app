ed6c4c787c5a5fd2147e64adc35221a6f4aa6eb5
Chu A. <chu.amk22@gmail.com>
2026-01-08 18:42:35 +0100

    refactor: replace hardcoded navigation links with NAV_LINKS constant

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router";
+import { NAV_LINKS } from "../utils/constants";
 
 const Header = () => {
-  const navStyle =
-    "text-white font-semibold hover:text-yellow-100 transition-colors duration-200";
-  const buttonStyle =
-    "px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition-all duration-200";
+  // const buttonStyle =
+  //   "px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition-all duration-200";
 
   return (
     <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg sticky top-0 z-10 ">
@@ -21,31 +20,18 @@ const Header = () => {
           {/* Navigation */}
           <nav>
             <ul className="flex gap-8 list-none">
-              <li>
-                <Link to="/" className={navStyle}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/recipes" className={navStyle}>
-                  Recipes
-                </Link>
-              </li>
-              <li>
-                <Link to="/favorites" className={navStyle}>
-                  Favorites
-                </Link>
-              </li>
-              <li>
-                <Link to="/community" className={navStyle}>
-                  Community
-                </Link>
-              </li>
-              <li>
-                <Link to="/signin" className={buttonStyle}>
-                  Sign In
-                </Link>
-              </li>
+              {NAV_LINKS.map((link) => {
+                return (
+                  <li key={link.to}>
+                    <Link
+                      to={link.to}
+                      className="text-white font-semibold hover:text-yellow-100 transition-colors duration-200"
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
