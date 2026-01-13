bd1dc5a3d8301754ab2830cfa69058106c525b93
Chu A. <chu.amk22@gmail.com>
2026-01-13 08:39:05 +0100

    feat(ui): implement mobile view for Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router";
 import { NAV_LINKS } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   // const buttonStyle =
   //   "px-4 py-2 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition-all duration-200";
 
   return (
-    <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg sticky top-0 z-10 ">
+    <header className="bg-linear-to-r from-gray-900 to-gray-800 shadow-lg sticky top-0 z-99 ">
       <div className="max-w-7xl mx-auto px-4 py-6">
-        <div className="flex items-center justify-between">
+        <div className="hidden md:flex items-center justify-between">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2">
             <div className="text-3xl font-bold text-white">
@@ -25,7 +27,7 @@ const Header = () => {
                   <li key={link.to}>
                     <Link
                       to={link.to}
-                      className="text-white font-semibold hover:text-yellow-100 transition-colors duration-200"
+                      className="text-white font-semibold hover:text-orange-400 transition-colors duration-200"
                     >
                       {link.label}
                     </Link>
@@ -35,6 +37,40 @@ const Header = () => {
             </ul>
           </nav>
         </div>
+
+        {/* Mobile Menu Button */}
+        <div className="flex items-center justify-between md:hidden">
+          <button className="text-white" onClick={() => setIsOpen(!isOpen)}>
+            <svg
+              className="w-8 h-8 text-orange-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+              />
+            </svg>
+          </button>
+          {/* Logo */}
+          <Link to="/" className="flex items-center gap-2">
+            <div className="text-3xl font-bold text-white">
+              <img className="w-10 h-10" src="./hero.svg" alt="" />
+            </div>
+            <h1 className="text-2xl font-bold text-white">Foodie</h1>
+          </Link>
+        </div>
+
+        {/* Mobile Menu Overlay */}
+        {isOpen && (
+          <div
+            className="fixed inset-0 bg-black/50 z-40 lg:hidden"
+            onClick={() => setIsOpen(false)}
+          />
+        )}
       </div>
     </header>
   );
