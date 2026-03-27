7bca9356daeb3d07372755a84ba9609a86df960a
Chu A. <chu.amk22@gmail.com>
2026-03-27 15:30:36 +0100

    feat: add routes for Favorites and Community pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import RecipesPage from "./pages/RecipesPage";
+import FavouritesPage from "./pages/FavouritesPage";
+import Community from "./pages/Community";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path="/" Component={LandingPage}></Route>
         <Route path="/recipes" Component={RecipesPage}></Route>
+        <Route path="/favorites" Component={FavouritesPage}></Route>
+        <Route path="/community" Component={Community}></Route>
       </Routes>
     </BrowserRouter>
   );
